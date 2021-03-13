6d8c43c21db3d698430b999004edc789d5c9430c
Schtinguerch <schtinguerch@gmail.com>
2021-03-13 20:03:26 +0500

    small refactoring + added new hand settings

--- WPFMeteroWindow/Tools/SetColor.cs
+++ WPFMeteroWindow/Tools/SetColor.cs
@@ -9,53 +9,44 @@ namespace WPFMeteroWindow
         Light,
         Dark
     }
+
     public static class SetColor
     {
-        public static void FirstColor(string color)
-        {
+        public static void FirstColor(string color) =>
             Settings.Default.MainBackground = color;
-        }
 
-        public static void SecondColor(string color)
-        {
+        public static void SecondColor(string color) =>
             Settings.Default.SecondBackground = color;
-        }
 
-        public static void CommandLineFirstColor(string color)
-        {
-            Settings.Default.ThirdBackground = color;
-        }
+        public static void CommandLineFirstColor(string color) =>
+            Settings.Default.ThirdBackground = color;   
 
-        public static void CommandLineSecondColor(string color)
-        {
+        public static void CommandLineSecondColor(string color) =>
             Settings.Default.ThirdSecBackground = color;
-        }
 
-        public static void KeyboardBackground(string color)
-        {
+        public static void KeyboardBackground(string color) =>
             Settings.Default.KeyboardBackgroundColor = color;
-        }
 
-        public static void KeyboardFontColor(string color)
-        {
+        public static void KeyboardFontColor(string color) =>
             Settings.Default.KeyboardFontColor = color;
-        }
-        
 
-        public static void KeyboardBorder(string color)
-        {
+        public static void KeyboardBorder(string color) =>
             Settings.Default.KeyboardBorderColor = color;
-        }
 
-        public static void KeyboardHighlight(string color)
-        {
+        public static void KeyboardHighlight(string color) =>
             Settings.Default.KeyboardHighlightColor = color;
-        }
 
-        public static void KeyboardErrorHighlight(string color)
-        {
+        public static void KeyboardErrorHighlight(string color) =>
             Settings.Default.KeyboardErrorHighlightColor = color;
-        }
+
+        public static void Hands(string color) =>
+            Settings.Default.HandsColor = color;
+
+        public static void HandsThickness(string thickness) =>
+            Settings.Default.HandsThickness = thickness;
+
+        public static void HandsThickness(double thickness) =>
+            Settings.Default.HandsThickness = thickness.ToString();
 
         public static void WindowColor(string color)
         {
