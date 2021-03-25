bbf610120e4ec292654034917a5d84477e1ed588
Schtinguerch <schtinguerch@gmail.com>
2021-03-25 18:29:32 +0500

    Common Discord RPC class

--- WPFMeteroWindow/Tools/Common/Intermediary.cs
+++ WPFMeteroWindow/Tools/Common/Intermediary.cs
@@ -6,6 +6,8 @@ namespace WPFMeteroWindow
     {
         public static MainWindow App { get; set; }
         
+        public static DiscordManager RichPresentManager { get; set; }
+        
         public static (string[][] keys, Button[] buttons) KeyboardData { get; set; }
         
         public static int KeyboardCharIndex { get; set; }
