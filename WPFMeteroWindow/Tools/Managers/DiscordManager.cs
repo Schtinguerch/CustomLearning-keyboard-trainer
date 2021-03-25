bc264185e5e1189d60a377025756113d884db575
Schtinguerch <schtinguerch@gmail.com>
2021-03-25 18:28:35 +0500

    app upd: added Discord rich presence support - RPC

---
+++ WPFMeteroWindow/Tools/Managers/DiscordManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using DiscordRPC;
+using DiscordRPC.Logging;
+using WPFMeteroWindow.Properties;
+
+namespace WPFMeteroWindow
+{
+    public class DiscordManager
+    {
+        private DiscordRpcClient Client { get; set; }
+
+        public void Initialize()
+        {
+            Client = new DiscordRpcClient("824522754971402241");
+            Client.Logger = new ConsoleLogger()
+            {
+                Level = LogLevel.Warning
+            };
+
+            Client.OnReady += (s, e) =>
+            {
+                LogManager.Log($"Received ready from user: {e.User.Username}");
+            };
+            Client.OnPresenceUpdate += (s, e) =>
+            {
+                LogManager.Log($"Received update: {e.Presence}");
+            };
+
+            Client.Initialize();
+        }
+
+        public void Update(string lessonName, string status, string imageKey)
+        {
+            Client.SetPresence(new RichPresence()
+            {
+                Details = lessonName,
+                State = status,
+                Assets = new Assets()
+                {
+                    LargeImageKey = imageKey,
+                    LargeImageText = "",
+                    SmallImageKey = ""
+                }
+            });
+        }
+
+        public void Update()
+        {
+            Update(Settings.Default.LessonName, "Typing", "");
+        }
+    }
+}
---
+++ WPFMeteroWindow/Tools/Managers/LogManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using MessageBox = System.Windows.MessageBox;
+
+namespace WPFMeteroWindow
+{
+    public static class LogManager
+    {
+        public static List<string> LogData { get; private set; } = new List<string>();
+        
+        public static void Log(string log)
+        {
+            LogData.Add($"{DateTime.Now.ToString()}: {log};");
+        }
+
+        public static void ShowLogList()
+        {
+            var message = "";
+            foreach (var logItem in LogData)
+                message += logItem + '\n';
+
+            MessageBox.Show(message);
+        }
+    }
+}
