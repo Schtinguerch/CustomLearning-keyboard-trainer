ae93f31fbadcd8539b128123b4fc0b381fbaf237
Schtinguerch <schtinguerch@gmail.com>
2021-05-02 19:46:23 +0500

    new: window page with logic for lesson editor class

---
+++ WPFMeteroWindow/Resources/pages/LessonEditorPage.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using WPFMeteroWindow.Properties;
+using Path = System.IO.Path;
+using Microsoft.Win32;
+using Localization = WPFMeteroWindow.Resources.localizations.Resources;
+
+namespace WPFMeteroWindow.Resources.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для LessonEditorPage.xaml
+    /// </summary>
+    public partial class LessonEditorPage : Page
+    {
+        private LessonEditor _editor;
+
+        public LessonEditorPage()
+        {
+            InitializeComponent();
+            LessonNameTextBox.Focus();
+
+            Intermediary.LessonPage = this;
+
+            if (Settings.Default.IsOpenUnderCourse)
+            {
+                var fileName = Settings.Default.LessonInCourseFileName;
+                if (fileName == "empty")
+                    NewLesson();
+                else
+                {
+                    _editor = new LessonEditor(fileName);
+                    EditorTitleTextBox.Text = $"{Path.GetFileName(fileName)} - {Localization.uLessonEditor}";
+                    DisplayDataFromEditor();
+                }
+            }
+            else
+            {
+                EditorTitleTextBox.Text = Path.GetFileName(Settings.Default.LoadedLessonFile) + $" - {Localization.uLessonEditor}";
+                _editor = new LessonEditor(Settings.Default.LoadedLessonFile);
+                DisplayDataFromEditor();
+            }
+        }
+
+        private void DisplayDataFromEditor()
+        {
+            LessonNameTextBox.Text = _editor.LessonName;
+            LessonCpmTextBox.Text = _editor.NecessaryCPM.ToString();
+            LessonMaxMistakesTextBox.Text = _editor.MaxAcceptableMistakes.ToString();
+            LessonDataTextBox.Text = _editor.LessonText;
+        }
+
+        private void SaveLesson()
+        {
+            _editor.LessonName = LessonNameTextBox.Text;
+            _editor.NecessaryCPM = Convert.ToInt32(LessonCpmTextBox.Text);
+            _editor.MaxAcceptableMistakes = Convert.ToInt32(LessonMaxMistakesTextBox.Text);
+            _editor.LessonText = LessonDataTextBox.Text;
+            
+            _editor.WriteDataOnFile();
+        }
+
+        private void OpenLesson()
+        {
+            var openDialog = new OpenFileDialog()
+            {
+                DefaultExt = ".lml",
+                Filter = "LML-Files|*.lml",
+                Multiselect = false
+            };
+
+            if (openDialog.ShowDialog() == true)
+            {
+                EditorTitleTextBox.Text = Path.GetFileName(openDialog.FileName) + $" - {Localization.uLessonEditor}";
+                _editor = new LessonEditor(openDialog.FileName);
+                DisplayDataFromEditor();
+            }
+        }
+
+        private void NewLesson()
+        {
+            _editor = new LessonEditor();
+
+            EditorTitleTextBox.Text = $"New lesson - {Localization.uLessonEditor}";
+            LessonNameTextBox.Text = LessonCpmTextBox.Text = LessonMaxMistakesTextBox.Text = LessonDataTextBox.Text = "";
+        }
+
+        private void SaveLessonButton_OnClick(object sender, RoutedEventArgs e) =>
+            SaveLesson();
+        
+        private void OpenLessonButton_OnClick(object sender, RoutedEventArgs e) =>
+            OpenLesson();
+
+        private void LessonEditorPage_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+                PageManager.HidePages();
+
+            else
+            {
+                if (AppManager.IsComboKeyDown(e, Key.LeftAlt, Key.O))
+                    OpenLesson();
+
+                else if (AppManager.IsComboKeyDown(e, Key.LeftAlt, Key.N))
+                    NewLesson();
+
+                else if (AppManager.IsComboKeyDown(e, Key.LeftAlt, Key.S))
+                    SaveLesson();
+            }
+        }
+
+        private void ClosePage()
+        {
+            if (Settings.Default.IsOpenUnderCourse)
+                Intermediary.CoursePage.CloseLessonEditorPage();
+            else
+                PageManager.HidePages();
+        }
+
+        private void CancelLessonButton_OnClick(object sender, RoutedEventArgs e) =>
+            ClosePage();
+
+        private void NewLessonButton_OnClick(object sender, RoutedEventArgs e) =>
+            NewLesson();
+    }
+}
