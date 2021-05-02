14cde5f1c4780a755b8e18d4fbd4f2505b8fe2a2
Schtinguerch <schtinguerch@gmail.com>
2021-05-02 19:48:59 +0500

    upd: new extentions (lml-format converter for editors and line-selector for textboxes)

--- WPFMeteroWindow/Tools/Managers/AppManager.cs
+++ WPFMeteroWindow/Tools/Managers/AppManager.cs
@@ -1,8 +1,10 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Input;
 using WPFMeteroWindow.Properties;
 using System.Collections.Generic;
+using System.Windows.Controls;
 using Application = System.Windows.Application;
 using KeyEventArgs = System.Windows.Input.KeyEventArgs;
 
@@ -82,6 +84,18 @@ namespace WPFMeteroWindow
             return val;
         }
 
+        public static string ToLmlFormat(this string s)
+        {
+            var val = s;
+
+            val = val.Replace("'", ";ap;");
+            val = val.Replace("\"", ";qt;");
+            val = val.Replace("{", ";opbr;");
+            val = val.Replace("}", ";clbr;");
+
+            return val;
+        }
+
         public static bool IsInRange(this int number, int a, int b) => ((number >= a) && (number <= b));
 
         public static bool IsEqualOr(this int number, params int[] numbers)
@@ -98,6 +112,41 @@ namespace WPFMeteroWindow
             }
 
             return value;
+        }
+
+        public static void SelectLine(this TextBox textBox)
+        {
+            int charIndex = textBox.CaretIndex;
+            int startPoint = charIndex;
+
+            if (textBox.CaretIndex == textBox.Text.Length - 1)
+                startPoint -= 3;
+
+            while ((textBox.Text[startPoint] != '\n') && (startPoint > 0))
+                startPoint--;
+
+            if ((startPoint != 0) && (startPoint < textBox.Text.Length))
+                startPoint++;
+
+            int
+                endPoint = startPoint + 1,
+                selectLength = 0;
+
+            try
+            {
+                while ((textBox.Text[endPoint] != '\n') && (endPoint < textBox.Text.Length))
+                {
+                    endPoint++;
+                    selectLength++;
+                }
+
+                textBox.Select(startPoint, selectLength + 1);
+            }
+
+            catch
+            {
+                textBox.Select(startPoint, textBox.Text.Length - startPoint);
+            }
         }
 
         public static bool IsComboKeyDown(KeyEventArgs e, params Key[] keys)
