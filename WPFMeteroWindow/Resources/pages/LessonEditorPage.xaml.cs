42442460fa449dce57888124f6db351a702e8721
Schtinguerch <schtinguerch@gmail.com>
2021-05-05 15:18:49 +0500

    bug-fix: lesson editors close after pressing any key

--- WPFMeteroWindow/Resources/pages/LessonEditorPage.xaml.cs
+++ WPFMeteroWindow/Resources/pages/LessonEditorPage.xaml.cs
@@ -96,7 +96,7 @@ namespace WPFMeteroWindow.Resources.pages
 
         private void LessonEditorPage_OnKeyDown(object sender, KeyEventArgs e)
         {
-            if (e.Key == Key.Escape || !Settings.Default.IsOpenUnderCourse)
+            if (e.Key == Key.Escape && !Settings.Default.IsOpenUnderCourse)
                 PageManager.HidePages();
 
             else
