4d93035d4a8648bf0f7f80be409e4d35b6c2a345
Schtinguerch <schtinguerch@gmail.com>
2021-05-06 15:40:43 +0500

    upd: log text become string variable, new methods to show logs by message text on the bottom of window

--- WPFMeteroWindow/Tools/Managers/LogManager.cs
+++ WPFMeteroWindow/Tools/Managers/LogManager.cs
@@ -7,19 +7,34 @@ namespace WPFMeteroWindow
     public static class LogManager
     {
         public static List<string> LogData { get; private set; } = new List<string>();
+
+        private static string lastLog;
+
+        public static string LogText
+        {
+            get
+            {
+                var message = "";
+                foreach (var logItem in LogData)
+                    message += logItem + '\n';
+
+                return message;
+            }
+        }
         
         public static void Log(string log)
         {
             LogData.Add($"{DateTime.Now.ToString()}: {log};");
+            lastLog = log;
         }
 
-        public static void ShowLogList()
-        {
-            var message = "";
-            foreach (var logItem in LogData)
-                message += logItem + '\n';
+        public static void ShowLogListViaMessageBox() =>
+            MessageBox.Show(LogText);
 
-            MessageBox.Show(message);
-        }
+        public static void ShowLastLog() => 
+            Intermediary.App.ShowMessage(lastLog);
+
+        public static void ShowMessage(string message) =>
+            Intermediary.App.ShowMessage(message);
     }
 }
