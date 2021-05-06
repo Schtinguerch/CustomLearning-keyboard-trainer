60c9bbb4b2612b35956e44220e042023ca16400b
Schtinguerch <schtinguerch@gmail.com>
2021-05-06 15:36:00 +0500

    add: new single-word commands

--- WPFMeteroWindow/Resources/pages/CommandLinePage.xaml.cs
+++ WPFMeteroWindow/Resources/pages/CommandLinePage.xaml.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Linq;
 using WPFMeteroWindow.Commands;
+using WPFMeteroWindow.Properties;
 using CommandManager = CommandMakerLibrary.CommandManager;
 using ICommand = CommandMakerLibrary.ICommand;
 
@@ -33,11 +33,51 @@ namespace WPFMeteroWindow.Resources.pages
             Intermediary.RichPresentManager.Update("Command line", "App management...", "");
         }
 
+        private string AfterProcessedMacroses()
+        {
+            var text = CommandTextBox.Text;
+
+            if (text.Contains("!save"))
+            {
+                Settings.Default.Save();
+                text = text.Replace("!save", "");
+            }
+            else if (text.Contains("!discard"))
+            {
+                Settings.Default.Reload();
+                text = text.Replace("!discard", "");
+
+                SetUnAutoColors();
+            }
+            else if (text.Contains("!default"))
+            {
+                Settings.Default.Reset();
+                text = text.Replace("!default", "");
+
+                SetUnAutoColors();
+            }
+
+            return text;
+        }
+
+        private void SetUnAutoColors()
+        {
+            KeyboardManager.LoadKeyboardData(Settings.Default.KeyboardLayoutFile);
+            SetFont.MainLetters(Settings.Default.LessonLettersFont);
+
+            SetColor.WindowColor(Settings.Default.ColorSchemeResourceDictionary.Split(new[] { '/' }).Last()
+                .Split(new[] { '.' })[0]);
+            SetColor.ColorScheme(Settings.Default.ThemeResourceDictionary.Contains("BaseLight") ? Theme.Light : Theme.Dark);
+        }
+
         private void CommandLinePage_OnKeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
             {
+                CommandTextBox.Text = AfterProcessedMacroses();
+                
                 var expressions = CommandTextBox.Text.Split(new char[] {';'}, StringSplitOptions.RemoveEmptyEntries).ToList();
+
                 foreach (var expression in expressions)
                 {
                     var tokens =
