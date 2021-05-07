96249332e99f341d74b9ae650b860cfac1c4172f
Schtinguerch <schtinguerch@gmail.com>
2021-05-07 16:08:21 +0500

    delete: unused usings

--- WPFMeteroWindow/Tools/Managers/KeyboardManager.cs
+++ WPFMeteroWindow/Tools/Managers/KeyboardManager.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
-using WPFMeteroWindow.Properties;
-using LmlLibrary;
+﻿using System.Windows.Controls;
 
 namespace WPFMeteroWindow
 {
