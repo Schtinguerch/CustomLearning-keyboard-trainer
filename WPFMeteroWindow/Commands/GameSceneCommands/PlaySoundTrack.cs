4c05206aedce15b9618a90c8e260db110d728b75
Schtinguerch <schtinguerch@gmail.com>
2021-06-14 11:24:57 +0500

    add: new command for ScriptMaker, sound loader

---
+++ WPFMeteroWindow/Commands/GameSceneCommands/PlaySoundTrack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using ScriptMaker;
+using WPFMeteroWindow.Properties;
+
+namespace WPFMeteroWindow.Commands
+{
+    public class PlaySoundTrack : Command
+    {
+        public override string Name { get; set; } = "play";
+        
+        public override void Run(List<string> arguments, object processingObject = null)
+        {
+            SetAdditional(arguments);
+            var mediaElement = processingObject as MediaElement;
+
+            mediaElement.LoadedBehavior = MediaState.Manual;
+            mediaElement.Source = new Uri(UnitedStringArgument, UriKind.Absolute);
+
+            mediaElement.Volume = Settings.Default.MusicVolume;
+        }
+    }
+}
