489b46c6f98e107d24ef6f71dc4f6fc79adfbca2
Schtinguerch <schtinguerch@gmail.com>
2021-06-14 11:25:18 +0500

    add: new command for ScriptMaker, setter tapping circle time

---
+++ WPFMeteroWindow/Commands/GameSceneCommands/SetCircleShowTime.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using WPFMeteroWindow.Properties;
+using ScriptMaker;
+
+namespace WPFMeteroWindow.Commands
+{
+    public class SetCircleShowTime : Command
+    {
+        public override string Name { get; set; } = "showTime";
+
+        public override void Run(List<string> arguments, object processingObject = null) =>
+            Settings.Default.TapperingMilliseconds = Convert.ToDouble(arguments[0]);
+    }
+}
