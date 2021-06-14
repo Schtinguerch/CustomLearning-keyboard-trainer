54075f96fd3d3285366d42e16c4bc73f7918491f
Schtinguerch <schtinguerch@gmail.com>
2021-06-14 11:25:40 +0500

    add: new command for ScriptMaker, setter max size of tappering circle

---
+++ WPFMeteroWindow/Commands/GameSceneCommands/SetMaxTapperingRadius.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using ScriptMaker;
+using WPFMeteroWindow.Properties;
+
+namespace WPFMeteroWindow.Commands
+{
+    public class SetMaxTapperingRadius : Command
+    {
+        public override string Name { get; set; } = "auraRadius";
+
+        public override void Run(List<string> arguments, object processingObject = null) =>
+            Settings.Default.MaxTapperingAuraSize = Convert.ToDouble(arguments[0]) * 2;
+    }
+}
