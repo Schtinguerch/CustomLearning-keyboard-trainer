bfc5cd19f9b03bf98e0ddf53420e52c280a113b3
Schtinguerch <schtinguerch@gmail.com>
2021-07-29 15:53:05 +0300

    add: logic interaction for classic text input (binding with ITextInputControl properties)

---
+++ WPFMeteroWindow/Controls/TextInputControls/Classic/Classic.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using WPFMeteroWindow.Controls;
+
+namespace WPFMeteroWindow.Controls.TextInputControls.Classic
+{
+    /// <summary>
+    /// Логика взаимодействия для Classic.xaml
+    /// </summary>
+    public partial class Classic : Page, ITextInputControl
+    {
+        private int TypeCount = 0;
+
+        public Classic()
+        {
+            InitializeComponent();
+
+            LessonManager.TextInputPresenter.TextInputWay = this;
+            AppManager.InitializeApplication();
+        }
+
+        public string DoneText
+        {
+            get => InputTextBox.Text;
+            set 
+            {
+                InputTextBox.Text = value;
+                TypeCount++;
+
+                if (TypeCount == 200)
+                {
+                    AllLessonTextBox.Text = AllLessonTextBox.Text.Remove(0, TypeCount);
+                    InputTextBox.Text = "";
+
+                    TypeCount = 0;
+                }
+            } 
+        }
+
+        public string LeftText
+        {
+            get;
+            set;
+        }
+
+        public string AllText
+        {
+            get => AllLessonTextBox.Text;
+            set => AllLessonTextBox.Text = value;
+        }
+
+        public string ErrorText
+        {
+            get;
+            set;
+        }
+
+        public void LoadText(string text)
+        {
+            AllText = LeftText = text;
+            DoneText = ErrorText = "";
+            TypeCount = 0;
+        }
+    }
+}
