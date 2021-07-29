ddad529d4a89ca70e517e1ed81fede78e329eeda
Schtinguerch <schtinguerch@gmail.com>
2021-07-29 15:54:30 +0300

    logic interaction for single line with static caret text input (binding with ITextInputControl properties)

---
+++ WPFMeteroWindow/Controls/TextInputControls/SingleLineWithStaticCaret/SingleLineWithStaticCaret.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using WPFMeteroWindow.Controls;
+
+namespace WPFMeteroWindow.Controls.TextInputControls.SingleLineWithStaticCaret
+{
+    /// <summary>
+    /// Логика взаимодействия для SingleLineWithStaticCaret.xaml
+    /// </summary>
+    public partial class SingleLineWithStaticCaret : Page, ITextInputControl
+    {
+        public SingleLineWithStaticCaret()
+        {
+            InitializeComponent();
+
+            LessonManager.TextInputPresenter.TextInputWay = this;
+            AppManager.InitializeApplication();
+        }
+
+        public string DoneText
+        {
+            get => inputTextBox.Text;
+            set => inputTextBox.Text = value;
+        }
+
+        public string LeftText
+        {
+            get => inputTextBlock.Text;
+            set => inputTextBlock.Text = value;
+        }
+
+        public string AllText { get; set; }
+        public string ErrorText
+        {
+            get => errorInputTextBlock.Text;
+            set => errorInputTextBlock.Text = value;
+        }
+
+        public void LoadText(string text)
+        {
+            AllText = LeftText = text;
+            DoneText = ErrorText = "";
+        }
+    }
+}
