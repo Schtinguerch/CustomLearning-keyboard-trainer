b81645e8774e9bdd5c395ffdcadc728a19c001fb
Schtinguerch <schtinguerch@gmail.com>
2021-07-29 15:57:47 +0300

    upd: moving logic of text input presentation to separate classes and frames, implementing the new interface.
    Love polymorphism :)

---
+++ WPFMeteroWindow/Controls/TextInputControls/ITextInputControl.cs
@@ -0,0 +1,11 @@
+﻿namespace WPFMeteroWindow.Controls
+{
+    public interface ITextInputControl
+    {
+        string DoneText { get; set; }
+        string LeftText { get; set; }
+        string AllText { get; set; }
+        string ErrorText { get; set; }
+        void LoadText(string text);
+    }
+}
