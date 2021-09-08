48606ba8f444c609a54541c56293d94ff7614ac5
Schtinguerch <schtinguerch@gmail.com>
2021-09-08 23:05:58 +0500

    add: extention to clone WPF controls

--- WPFMeteroWindow/Tools/Common/Intermediary.cs
+++ WPFMeteroWindow/Tools/Common/Intermediary.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
 using WPFMeteroWindow.Resources.pages;
 
 namespace WPFMeteroWindow
@@ -22,5 +25,16 @@ namespace WPFMeteroWindow
         public static int KeyboardCharIndex { get; set; }
         
         public static int KeyboardModifierIndex { get; set; }
+
+
+        public static T GetCopy<T>(this T element) where T : UIElement
+        {
+            using (var ms = new MemoryStream())
+            {
+                XamlWriter.Save(element, ms);
+                ms.Seek(0, SeekOrigin.Begin);
+                return (T)XamlReader.Load(ms);
+            }
+        }
     }
 }
