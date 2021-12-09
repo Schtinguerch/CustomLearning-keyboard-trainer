e5c3bc0a1043e08f8033308649ef5583defd19ff
Schtinguerch <schtinguerch@gmail.com>
2021-12-09 18:53:34 +0500

    Add: manager of recent opened skin-configs (every item is styled by the skin-config)

---
+++ WPFMeteroWindow/Resources/pages/ActiveUsingConfigs.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+using WPFMeteroWindow.Properties;
+using WPFMeteroWindow.Controls;
+using Newtonsoft.Json;
+using Localization = WPFMeteroWindow.Resources.localizations.Resources;
+
+namespace WPFMeteroWindow.Resources.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ActiveUsingConfigs.xaml
+    /// </summary>
+    public partial class ActiveUsingConfigs : Page
+    {
+        private List<string> _previousConfigsData;
+
+        public ActiveUsingConfigs()
+        {
+            InitializeComponent();
+            ReinitializeSkinList();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e) =>
+           PageManager.HidePages();
+
+        private void ReinitializeSkinList()
+        {
+            ScrollListStackPanel.Children.Clear();
+
+            _previousConfigsData = JsonConvert.DeserializeObject<List<string>>(
+                File.ReadAllText(Settings.Default.RecentConfigs));
+
+            if (_previousConfigsData.Count > 0)
+                EmptyListTextBox.Visibility = Visibility.Hidden;
+
+            foreach (var layout in _previousConfigsData)
+                ScrollListStackPanel.Children.Insert(0, new ConfigItem(layout)
+                {
+                    Margin = new Thickness(0, 20, 0, 0)
+                });
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            UserConfigManager.ImportConfigViaExplorer();
+            var newRecentConfigsData = JsonConvert.DeserializeObject<List<string>>(
+                File.ReadAllText(Settings.Default.RecentConfigs));
+
+            if (newRecentConfigsData.Count == _previousConfigsData.Count)
+                return;
+
+            ReinitializeSkinList();
+        }
+
+        private void DeleteAllButton_Click(object sender, RoutedEventArgs e)
+        {
+            ScrollListStackPanel.Children.Clear();
+            _previousConfigsData?.Clear();
+
+            File.WriteAllText(Settings.Default.RecentConfigs, "[]");
+            EmptyListTextBox.Visibility = Visibility.Visible;
+        }
+
+        private void wrapPanel_DragEnter(object sender, DragEventArgs e)
+        {
+            DragAndDropBackground.Opacity = 0.5d;
+
+            EmptyListTextBox.VerticalAlignment = VerticalAlignment.Bottom;
+            EmptyListTextBox.Text = Localization.uLoadNewStyles;
+            EmptyListTextBox.Visibility = Visibility.Visible;
+        }
+
+        private void wrapPanel_DragLeave(object sender, DragEventArgs e)
+        {
+            DragAndDropBackground.Opacity = 0d;
+
+            EmptyListTextBox.VerticalAlignment = VerticalAlignment.Center;
+            EmptyListTextBox.Text = Localization.uEmptyList;
+            EmptyListTextBox.Visibility = Visibility.Hidden;
+        }
+
+        private void wrapPanel_Drop(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop))
+                return;
+
+            var configs = e.Data.GetData(DataFormats.FileDrop) as string[];
+
+            if (configs == null)
+                return;
+
+            foreach (var config in configs)
+                UserConfigManager.AddToRecent(config);
+
+            ReinitializeSkinList();
+            wrapPanel_DragLeave(null, null);
+        }
+    }
+}
