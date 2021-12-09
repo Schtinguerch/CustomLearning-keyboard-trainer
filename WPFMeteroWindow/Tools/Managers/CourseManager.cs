2c9fcb8e2ebb9759b9ac72d88333c60f0099b46e
Schtinguerch <schtinguerch@gmail.com>
2021-12-09 18:41:52 +0500

    Add: new type of courses - dilctionary to open lessons via Random choosement Add: working with recent opened courses list

--- WPFMeteroWindow/Tools/Managers/CourseManager.cs
+++ WPFMeteroWindow/Tools/Managers/CourseManager.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Linq;
 using System.IO;
 using System.Windows;
+using System.Windows.Controls;
 using System.Collections.Generic;
+using System.Windows.Media;
 using LmlLibrary;
 using WPFMeteroWindow.Properties;
+using Newtonsoft.Json;
 
 namespace WPFMeteroWindow
 {
@@ -25,7 +29,19 @@ namespace WPFMeteroWindow
                 
                     StatisticsManager.ReloadTimer();
                     if (Settings.Default.IsCourseOpened)
-                        LessonManager.LoadLesson(Lessons[value]);
+                    {
+                        int choosenLesson = 0;
+
+                        if (Settings.Default.CourseLessonNumber == value && Settings.Default.IsDictionary)
+                            choosenLesson = new Random().Next(0, LessonsCount);
+                        else
+                            choosenLesson = value;
+
+                        LessonManager.LoadLesson(Lessons[choosenLesson]);
+
+                        Settings.Default.LessonInCourseFileName = Lessons[choosenLesson];
+                        Settings.Default.Save();
+                    }
                     else
                         LessonManager.LoadLesson(Settings.Default.LoadedLessonFile);
 
@@ -40,35 +56,102 @@ namespace WPFMeteroWindow
                 catch { }
             }
         }
-        
-        public static void LoadCourse(string filename)
+
+        public static void LoadCourse(string filename, bool skipUiLoadng = false)
         {
+            var courseType = "";
             if (filename.Contains(".lml"))
             {
                 while (!File.Exists(filename + "\\CourseLessons.lml"))
                     filename = new DirectoryInfo(filename).Parent.FullName;
             }
-            
+
             var reader = new Lml(filename + "\\CourseLessons.lml", Lml.Open.FromFile);
-            var lessonFiles =  AppManager.GetFileList(reader.GetArray("Course>LessonList"));
+            var fullName = new DirectoryInfo(filename).FullName;
 
-            for (int i = 0; i < lessonFiles.Count; i++)
-                lessonFiles[i] = new DirectoryInfo(filename).FullName + "\\" + lessonFiles[i];
+            var lessons = AppManager.GetFileList(reader.GetArray("Course>LessonList"), fullName);
+            var isDictionary = reader.GetAllData().Substring(0, 11) == "#dictionary";
 
-            Lessons = lessonFiles;
-            LessonsCount = Lessons.Count;
+            var courseName = reader.GetString("Course>Name");
+            if (isDictionary)
+                courseType = "#dictionary";
 
-            LessonManager.LoadLesson(lessonFiles[Settings.Default.CourseLessonNumber]);
-            
-            Settings.Default.LoadedLessonFile = lessonFiles[Settings.Default.CourseLessonNumber];
-            Settings.Default.LoadedCourseFile = filename;
-            Settings.Default.CourseName = reader.GetString("Course>Name");
-            Settings.Default.Save();
+            if (!skipUiLoadng)
+            {
+                Lessons = lessons;
+                LessonsCount = lessons.Count;
 
-            Intermediary.App.NextLessonButton.Visibility = Visibility.Visible;
-            Intermediary.App.PrevLessonButton.Visibility = Visibility.Visible;
+                LessonManager.LoadLesson(Lessons[Settings.Default.CourseLessonNumber]);
+                Intermediary.App.LoadCourseOrLessonButton.ContextMenu = CourseContextMenu();
 
-            CurrentLessonIndex = Settings.Default.CourseLessonNumber;
+                Settings.Default.CourseName = courseName;
+                Settings.Default.IsDictionary = isDictionary;
+
+                Settings.Default.LoadedLessonFile = Lessons[Settings.Default.CourseLessonNumber];
+                Settings.Default.LoadedCourseFile = filename;
+                Settings.Default.Save();
+
+                Intermediary.App.NextLessonButton.Visibility = Visibility.Visible;
+                Intermediary.App.PrevLessonButton.Visibility = Visibility.Visible;
+
+                CurrentLessonIndex = Settings.Default.CourseLessonNumber;
+            }
+
+            WriteDataToJson(
+                name: courseName,
+                type: courseType,
+                lessonCount: lessons.Count,
+                filename: filename);
+        }
+
+        public static void WriteDataToJson(string name, string type, int lessonCount, string filename)
+        {
+            var hasTheSameCourse = false;
+            var recentCources = JsonConvert.DeserializeObject<List<List<string>>>(
+                File.ReadAllText(Settings.Default.RecentCourcesPath));
+
+            foreach (var item in recentCources)
+            {
+                if (filename == item[3])
+                {
+                    hasTheSameCourse = true;
+                    break;
+                }
+            }
+
+            if (!hasTheSameCourse)
+                recentCources.Add(new List<string>()
+                {
+                    name,
+                    type,
+                    lessonCount.ToString(),
+                    filename,
+                });
+
+            File.WriteAllText(
+                Settings.Default.RecentCourcesPath,
+                JsonConvert.SerializeObject(recentCources, Formatting.Indented));
+        }
+
+        private static ContextMenu CourseContextMenu()
+        {
+            var contextMenu = new ContextMenu();
+            contextMenu.MaxHeight = 300d;
+            contextMenu.FontFamily = new FontFamily(Settings.Default.SummaryFont);
+
+            foreach (var lesson in Lessons)
+            {
+                var reader = new Lml(lesson, Lml.Open.FromFile);
+                var lessonName = reader.GetString("Lesson>Name");
+
+                var newMenuItem = new MenuItem { Header = lessonName };
+                newMenuItem.Click += (s, e) =>
+                   CurrentLessonIndex = contextMenu.Items.IndexOf(s as MenuItem);
+
+                contextMenu.Items.Add(newMenuItem);
+            }
+
+            return contextMenu;
         }
     }
 }
