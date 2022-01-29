3f0ba2df4644ab17c16b67879d0050145cbad71a
Schtinguerch <schtinguerch@gmail.com>
2022-01-29 20:31:14 +0500

    Add: passement indicators support

--- WPFMeteroWindow/Resources/pages/ChooseLessonMenu.xaml.cs
+++ WPFMeteroWindow/Resources/pages/ChooseLessonMenu.xaml.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Shapes;
 using WPFMeteroWindow.Properties;
 
 namespace WPFMeteroWindow.Resources.pages
@@ -15,49 +16,68 @@ namespace WPFMeteroWindow.Resources.pages
     {
         public ChooseLessonMenu(List<string> lessons)
         {
-            var dictionary = (ResourceDictionary)Application.LoadComponent(new Uri("\\Resources\\dictionaries\\Styles.xaml", UriKind.Relative));
-            var style = (Style)dictionary["SummaryTextStyle"];
+            var style = XamlManager.FindResource<Style>("SummaryTextStyle");
 
             InitializeComponent();
             LessonStackPanel.Children.Capacity = 5000;
+
+            if (lessons == null || lessons.Count == 0)
+                return;
 
             foreach (var lesson in lessons)
                 AddLesson(lesson, style);
         }
 
+        private SolidColorBrush _notPassedIndicatorBrush = XamlManager.FindResource<SolidColorBrush>("NotPassedIndicatorBrush");
+        private SolidColorBrush _passedIndicatorBrush = XamlManager.FindResource<SolidColorBrush>("PassedIndicatorBrush");
+        private SolidColorBrush _failedIndicatorBrush = XamlManager.FindResource<SolidColorBrush>("FailedIndicatorBrush");
+
         private void AddLesson(string filename, Style style)
         {
+            var stackPanel = new StackPanel() { Orientation = Orientation.Horizontal };
+            var indicator = new Ellipse() 
+            { 
+                Width = 8, 
+                Height = 8, 
+                Margin = new Thickness(20, 0, 10, 0),
+                Fill = _notPassedIndicatorBrush,
+            };
+
             var textBlock = new TextBlock()
             {
                 Text = OptimizedGetLessonName(filename),
                 Padding = new Thickness(10, 2, 10, 2),
+                Width = 400d,
                 Style = style,
                 FontSize = 18d,
             };
 
-            textBlock.MouseEnter += (s, e) => 
-            { 
-                textBlock.Background = 
+            stackPanel.MouseEnter += (s, e) => 
+            {
+                stackPanel.Background = 
                 new BrushConverter().ConvertFromString(Settings.Default.KeyboardHighlightColor) 
                 as SolidColorBrush; 
             };
 
-            textBlock.MouseLeave += (s, e) =>
+            stackPanel.MouseLeave += (s, e) =>
             {
-                textBlock.Background =
+                stackPanel.Background =
                 new BrushConverter().ConvertFromString(Settings.Default.MainBackground)
                 as SolidColorBrush;
             };
 
             textBlock.MouseDown += (s, e) =>
             {
-                CourseManager.CurrentLessonIndex = LessonStackPanel.Children.IndexOf(s as TextBlock);
+                CourseManager.CurrentLessonIndex = GetIndexOfTextBlock(s as TextBlock);
                 textBlock.Background =
                 new BrushConverter().ConvertFromString(Settings.Default.MainBackground)
                 as SolidColorBrush;
             };
 
-            LessonStackPanel.Children.Add(textBlock);
+            stackPanel.Children.Add(indicator);
+            stackPanel.Children.Add(textBlock);
+
+            LessonStackPanel.Children.Add(stackPanel);
         }
 
         private string OptimizedGetLessonName(string filename)
@@ -72,6 +92,49 @@ namespace WPFMeteroWindow.Resources.pages
                 return "...";
 
             return data.Substring(startIndex, length);
+        }
+
+        public void RefreshPassingIndicators()
+        {
+            foreach (var index in StatisticsManager.CourseMarks.FullyPassedLessons)
+            {
+                var stackPanel = LessonStackPanel.Children[index] as StackPanel;
+                var ellipse = stackPanel.Children[0] as Ellipse;
+
+                ellipse.Fill = _passedIndicatorBrush;
+            }
+
+            foreach (var index in StatisticsManager.CourseMarks.PartucularlyPassedLessons)
+            {
+                var stackPanel = LessonStackPanel.Children[index] as StackPanel;
+                var ellipse = stackPanel.Children[0] as Ellipse;
+
+                ellipse.Fill = _failedIndicatorBrush;
+            }
+        }
+
+        private void UserControl_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (IsVisible)
+                RefreshPassingIndicators();
+        }
+
+        private int GetIndexOfTextBlock(TextBlock textBlock)
+        {
+            int index = -1;
+
+            foreach (var child in LessonStackPanel.Children)
+            {
+                index++;
+
+                var stackPanel = child as StackPanel;
+                var txtBlock = stackPanel.Children[1] as TextBlock;
+
+                if (textBlock.Equals(txtBlock))
+                    return index;
+            }
+
+            return 0;
         }
     }
 }
