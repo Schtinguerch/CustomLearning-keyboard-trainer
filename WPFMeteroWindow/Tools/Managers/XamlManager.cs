2c9cbf8e1ff09bc269842326f468daff513347bf
Schtinguerch <schtinguerch@gmail.com>
2022-01-29 20:35:58 +0500

    Add: new simple manager for loading styles, colors from resource dictionaries without tonnes of code

---
+++ WPFMeteroWindow/Tools/Managers/XamlManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+
+namespace WPFMeteroWindow
+{
+    public static class XamlManager
+    {
+        private static string _baseFolder = "\\Resources\\dictionaries";
+        private static string _dictionaryFile = "Styles.xaml";
+
+        private static ResourceDictionary _dictionary =
+            (ResourceDictionary)Application.LoadComponent(
+                new Uri($"{_baseFolder}\\{_dictionaryFile}",
+                UriKind.Relative));
+
+        public static string DictionaryFile
+        {
+            get => _dictionaryFile;
+            set
+            {
+                _dictionaryFile = value;
+                _dictionary = (ResourceDictionary)
+                    Application.LoadComponent(
+                        new Uri($"{_baseFolder}\\{_dictionaryFile}", 
+                        UriKind.Relative));
+            }
+        }
+
+        public static T FindResource<T>(string key) 
+            where T : class => 
+            _dictionary[key] as T;
+    }
+}
