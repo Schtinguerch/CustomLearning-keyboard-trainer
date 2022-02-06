f07e5a16c6d55c5f12424de500a6ccb1f2e7d079
Schtinguerch <schtinguerch@gmail.com>
2022-02-06 17:25:26 +0500

    Add: controls to hold choosen shape for keyboard and mouse animations

---
+++ WPFMeteroWindow/Controls/LightingDonut.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+using WPFMeteroWindow.Properties;
+
+namespace WPFMeteroWindow.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для LightingDonut.xaml
+    /// </summary>
+    public partial class LightingDonut : UserControl
+    {
+        private Storyboard _splashStoryboard;
+        public void StartSplash()
+        {
+            if (!Settings.Default.EnableSplashAnimation)
+                return;
+
+            _splashStoryboard.Begin();
+        }
+
+        public Grid Shape
+        {
+            get => ShapeGrid;
+            set
+            {
+                var newShape = value.Copy();
+
+                MainGrid.Children.Clear();
+                MainGrid.Children.Add(newShape);
+            }
+        }
+
+        public bool RepeatForever
+        {
+            get => _splashStoryboard.RepeatBehavior == RepeatBehavior.Forever;
+            set
+            {
+                if (value)
+                    _splashStoryboard.RepeatBehavior = RepeatBehavior.Forever;
+                else 
+                    _splashStoryboard.RepeatBehavior = new RepeatBehavior(1.0);
+            }
+        }
+
+        public LightingDonut()
+        {
+            InitializeComponent();
+            _splashStoryboard = FindResource("SplashStoryboard") as Storyboard;
+        }
+    }
+}
