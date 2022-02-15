46c0f08f062465dba6815943cbb5f1f8a0c64368
Schtinguerch <schtinguerch@gmail.com>
2022-02-15 20:05:46 +0500

    Add: manager to change app's localization language

---
+++ WPFMeteroWindow/Tools/Managers/LanguageManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
+
+using Localization = WPFMeteroWindow.Resources.localizations.Resources;
+using WPFMeteroWindow.Properties;
+
+namespace WPFMeteroWindow
+{
+    public static class LanguageManager
+    {
+        public static readonly List<string> Languages = new List<string>()
+        {
+            Localization.uLanguageOfSystem,
+            "English",
+            "Русский",
+        };
+
+        private static readonly List<Action> _setLanguageActions = new List<Action>()
+        {
+            () => Thread.CurrentThread.CurrentUICulture = CultureInfo.CurrentCulture,
+            () => Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US"),
+            () => Thread.CurrentThread.CurrentUICulture = new CultureInfo("ru-RU"),
+        };
+
+        public static void SetLanguage(int index)
+        {
+            _setLanguageActions[index].Invoke();
+            Settings.Default.ChosenLanguageIndex = index;
+        }
+    }
+}
