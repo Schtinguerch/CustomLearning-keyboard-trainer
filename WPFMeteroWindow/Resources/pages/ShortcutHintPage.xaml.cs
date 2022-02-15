08d7214175286b9c2c186901260bede1a8eb6816
Schtinguerch <schtinguerch@gmail.com>
2022-02-15 20:08:19 +0500

    Add: new page for keyboard shortcut hints

---
+++ WPFMeteroWindow/Resources/pages/ShortcutHintPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using WPFMeteroWindow.Properties;
+using Localization = WPFMeteroWindow.Resources.localizations.Resources;
+
+namespace WPFMeteroWindow.Resources.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ShortcutHintPage.xaml
+    /// </summary>
+    public partial class ShortcutHintPage : Page
+    {
+        public ShortcutHintPage()
+        {
+            InitializeComponent();
+
+            if (Settings.Default.ItTypingTest)
+                TypingTestHintTextBox.Text = Localization.uBackToLessons;
+        }
+    }
+}
