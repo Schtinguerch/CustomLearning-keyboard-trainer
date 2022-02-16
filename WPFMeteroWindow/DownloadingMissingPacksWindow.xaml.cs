bd56da1891f12331f37490411f2dcb72ec754379
Schtinguerch <schtinguerch@gmail.com>
2022-02-16 16:20:15 +0500

    Add: window to show message (Downloading missing packages)

---
+++ WPFMeteroWindow/DownloadingMissingPacksWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Windows;
+
+
+namespace WPFMeteroWindow
+{
+    /// <summary>
+    /// Логика взаимодействия для DownloadingMissingPacksWindow.xaml
+    /// </summary>
+    public partial class DownloadingMissingPacksWindow : Window
+    {
+
+        public DownloadingMissingPacksWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
