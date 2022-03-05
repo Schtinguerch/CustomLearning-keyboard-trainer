89d23d62c04088822661e468ab5a948b24509dec
Schtinguerch <schtinguerch@gmail.com>
2022-03-05 12:02:02 +0500

    Add: icon for Hacker rank (140-160 WPM)

---
+++ WPFMeteroWindow/Resources/TypingSpeedRanks/RankGrids/Hacker.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFMeteroWindow.Resources.TypingSpeedRanks.RankGrids
+{
+    /// <summary>
+    /// Логика взаимодействия для Hacker.xaml
+    /// </summary>
+    public partial class Hacker : UserControl
+    {
+        public Hacker()
+        {
+            InitializeComponent();
+        }
+    }
+}
